19e281de9a9bd39f473a414663fe8cfc3c8f5643
Stella Tran <stellatranthi@hotmail.com>
2022-06-24 12:40:03 -0700

    Revised MQTT Broker methods

--- dashboard/v2_mqtt.py
+++ dashboard/v2_mqtt.py
@@ -7,31 +7,32 @@ from types import SimpleNamespace
 #Followed: http://www.steves-internet-guide.com/python-mqtt-publish-subscribe/
 
 class MQTT:
-    msgs = []
-    broker='35.183.154.210'
-    port = 1883
-    client = paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
-    
+
+    def __init__(self):
+        self.msgs = []
+        self.broker='35.183.154.210'
+        self.port = 1883
+        self.client = paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+        username = 'avaadmin'
+        password = 'SBrf,$TukF9Zd8:L'
+        self.client.username_pw_set(username, password)
+        self.client.on_message=self.on_message
+
     def on_message(self, client, userdata, message):
         x = json.loads(str(message.payload.decode("utf-8")), object_hook=lambda d: SimpleNamespace(**d))
+        print("X: ", x)
         self.msgs = x
 
-    username = 'avaadmin'
-    password = 'SBrf,$TukF9Zd8:L'
-
-    client.username_pw_set(username, password)
-
     def get_device_status(self, device):
-
         self.client.connect(self.broker, port=self.port)#connect
         self.client.loop_start() #start loop to process received messages
 
         self.client.subscribe(f'avagrows/device/client/{device}/deviceState')#subscribe
         self.client.publish(f'avagrows/device/server/{device}/devicecommand','{"command": 0}')#publish
-        time.sleep(1)
+        time.sleep(2)
         self.client.unsubscribe(f'avagrows/device/client/{device}/deviceState')#subscribe
 
-        time.sleep(3)
+        time.sleep(1)
 
         self.client.disconnect() #disconnect
         self.client.loop_stop()
@@ -41,7 +42,6 @@ class MQTT:
 
 
     def set_start_time(self, device, hour, minute):
-        self.client.on_message=self.on_message
         self.client.connect(self.broker, port=self.port)#connect
         self.client.loop_start() #start loop to process received messages
 
