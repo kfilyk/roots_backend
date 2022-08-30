0643dcb222b2c213a65c1bda1bd7a45ed1ad2fd8
kfilyk <kelvinfilyk@gmail.com>
2022-08-30 08:08:21 -0700

    Adding local changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,9 @@ import Router from './Router';
 const App = () => {
     
   useEffect(() => {
-    axios.defaults.baseURL = 'http://localhost:8000'; // use this to run locally
+    //axios.defaults.baseURL = 'http://0.0.0.0:8000'; // use this to run locally
+    axios.defaults.baseURL = 'https://avaroots.io:8000'; // use this to run on EC2
+
     axios.defaults.timeout = 10000;
   }, []);
 
