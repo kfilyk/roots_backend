82e113d24d981147efd53f965d0dc329d86e9260
Stella Tran <software@avagrows.com>
2022-10-12 16:06:27 -0700

    Oct 12th Pull

--- frontend/src/components/Analysis/Analysis.js
+++ frontend/src/components/Analysis/Analysis.js
@@ -3,11 +3,8 @@ import axios from 'axios';
 import Popup from "reactjs-popup";
 import menu_icon from "../../img/menu_icon.png";
 import 'chart.js/auto'
-//import {Chart as ChartJS, LinearScale, PointElement, Tooltip, Legend, TimeScale} from "chart.js";
 import {Bar, Line, Scatter} from 'react-chartjs-2';
 import './analysis.css';
-
-//ChartJS.register(LinearScale, PointElement, Tooltip, Legend, TimeScale);
 
 const Analysis = () => {
   const [erList, setExperimentReadingList] = useState([]);
@@ -21,40 +18,6 @@ const Analysis = () => {
   const [dates, setDates] = useState([]);
   const [podData, setPodData] = useState([]);
   const [heightData, setHeightData] = useState([]);
-
-  
-
-  /*const germinationCount = pr_list.map(a => a.germination_rate);
-  const labels = pr_list.map(b => b.id);
-*/
-
-  /* test graph 1 */
-  const [barData, setBarData] = useState({
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-    }]
-  })
-  
-
   
   // LOOK IN "urls.py":  /api/plants/, /api/pods/, etc...
   async function fetchExperimentReadings() {
@@ -80,7 +43,7 @@ const Analysis = () => {
 
   /* Trying to get the last height measurement */
   async function getAllGraphData() {
-    const maxHeight = podData['pods'][1]//['reading'][-1]
+    const maxHeight = podData.pods[0].reading.at(-1)//[1]//['reading'][-1]
 
     setHeightData(maxHeight)
   }
@@ -131,19 +94,6 @@ const Analysis = () => {
     }]
   };
 
-
-/*Test data to make any line graph render*/
-const lineDataTest = [1,2,3,4,5,6]
-const lineData = {
-  labels:['one','two','three','four','five','six'],
-  datasets: [
-    {
-      label:'test data',
-      data: lineDataTest
-    }
-  ]
-}
-
 const domeData = {
   labels: dates,
   datasets: [
@@ -163,15 +113,6 @@ const domeData = {
     {JSON.stringify(heightData)}
     <h3>Experiment ID: 80</h3>
     {JSON.stringify(podData)}
-    
-    
-    
-    
-    <h1>@@@@@@@</h1>
-
-    {JSON.stringify(erList)}
-    <h1>#######</h1>
-    {JSON.stringify(prList)}
 
     <Line data={domeData}
     options={{
@@ -250,10 +191,12 @@ const domeData = {
       }}/>
     </div>
     /*<div>
-      <Bar data={barData}/>
+    
+    <h1>@@@@@@@</h1>
 
-      <Line data={lineData}/>
-
+    {JSON.stringify(erList)}
+    <h1>#######</h1>
+    {JSON.stringify(prList)}
       {JSON.stringify(germinationCount)};
       {JSON.stringify(labels)};
       {JSON.stringify(dome)};
