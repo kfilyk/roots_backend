f1a539d7da9c9a6f1043468fb8e9329e210348d0
kfilyk <kelvinfilyk@gmail.com>
2022-11-22 12:55:20 -0800

    CORS ORIGIN RESTRICTIVE

--- plant_science_app/settings.py
+++ plant_science_app/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # having avaroots.io whitelisted/allowed shouldnt affect break anything...
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 if not CORS_ORIGIN_ALLOW_ALL:
     CORS_ALLOWED_ORIGINS = [
